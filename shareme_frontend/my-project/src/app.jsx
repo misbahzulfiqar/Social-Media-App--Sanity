2243d9f66c910e916c49bcf626c35e5c01018567
Misbah Zulfiqar <misbahzulfiqar.333ww@example.com>
2026-04-10 16:05:28 +0500

    fix login

--- shareme_frontend/my-project/src/app.jsx
+++ shareme_frontend/my-project/src/app.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
-import { Routes, Route, useNavigate } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Home from './container/Home'
 import Login from './components/Login'
 //assets
 const App = () => {
+  const userInfo = localStorage.getItem('user') !== 'undefined'
+    ? JSON.parse(localStorage.getItem('user'))
+    : null;
+  const isLoggedIn = Boolean(userInfo?.googleId);
 
   return (
     <Routes>
-      <Route path="login" element={<Login />} />
-      <Route path="/*" element={<Home />} />
+      <Route path="login" element={isLoggedIn ? <Navigate to="/" replace /> : <Login />} />
+      <Route path="/*" element={isLoggedIn ? <Home /> : <Navigate to="/login" replace />} />
     </Routes>
   )
   
